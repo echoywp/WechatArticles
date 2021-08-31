70a1ebbc5c2e2126810889e6f4b245440e00b4b1
ywp <csdd714342768@qq.com>
2021-08-31 17:47:02 +0800

    1.1.4

--- src/Wechat/WechatArticles.php
+++ src/Wechat/WechatArticles.php
@@ -5,22 +5,37 @@ class WechatArticles {
 
     public $content_url;
 
+    protected $html;
+
     public function index() {
-        $html = file_get_contents($this->content_url);
-        preg_match_all("/id=\"js_content\" style=\"visibility: hidden;\">(.*)<script/iUs",$html,$content,PREG_PATTERN_ORDER);
+        $this->html = file_get_contents($this->content_url);
+
+        return [
+            'content' => $this->getContent(),
+            'nickname' => $this->getNickname(),
+            'head_img' => $this->getHeadImg()
+        ];
+    }
+
+    //文章内容
+    protected function getContent() {
+        preg_match_all("/id=\"js_content\" style=\"visibility: hidden;\">(.*)<script/iUs", $this->html, $content, PREG_PATTERN_ORDER);
         $content = "<div id='js_content'>".$content[1][0];
         $content = str_replace("data-src","src",$content);
         $content = str_replace("preview.html","player.html",$content);
+        return $content;
+    }
 
-        preg_match_all('/var nickname = \"(.*?)\";/si',$html,$m);
-        $nickname = $m[1][0];//公众号昵称
-        preg_match_all('/var round_head_img = \"(.*?)\";/si',$html,$m);
-        $head_img = $m[1][0];//公众号头像
 
-        return [
-            'content' => $content,
-            'nickname' => $nickname,
-            'head_img' => $head_img
-        ];
+    //公众号头像
+    protected function getHeadImg() {
+        preg_match_all('/var round_head_img = \"(.*?)\";/si', $this->html, $m);
+        return array_key_exists(1, $m)? $m[1][0] : '';
+    }
+
+    //公众号昵称
+    protected function getNickname() {
+        preg_match_all('/var nickname = \"(.*?)\";/si', $this->html, $m);
+        return array_key_exists(1, $m)? $m[1][0] : '';
     }
 }
