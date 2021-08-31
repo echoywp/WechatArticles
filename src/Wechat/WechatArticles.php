f033da19b7a1bf2e62d62c5d78ab501f41651dc2
ywp <csdd714342768@qq.com>
2021-08-31 17:40:31 +0800

    1.1.3

--- src/Wechat/WechatArticles.php
+++ src/Wechat/WechatArticles.php
@@ -3,7 +3,24 @@ namespace Wechat;
 
 class WechatArticles {
 
+    public $content_url;
+
     public function index() {
-        echo '这是一个composer包第4版';
+        $html = file_get_contents($this->content_url);
+        preg_match_all("/id=\"js_content\" style=\"visibility: hidden;\">(.*)<script/iUs",$html,$content,PREG_PATTERN_ORDER);
+        $content = "<div id='js_content'>".$content[1][0];
+        $content = str_replace("data-src","src",$content);
+        $content = str_replace("preview.html","player.html",$content);
+
+        preg_match_all('/var nickname = \"(.*?)\";/si',$html,$m);
+        $nickname = $m[1][0];//公众号昵称
+        preg_match_all('/var round_head_img = \"(.*?)\";/si',$html,$m);
+        $head_img = $m[1][0];//公众号头像
+
+        return [
+            'content' => $content,
+            'nickname' => $nickname,
+            'head_img' => $head_img
+        ];
     }
 }
